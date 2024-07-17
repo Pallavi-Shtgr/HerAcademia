66bc3dbe26689c5b46bd8fe3b470bdeba4d5589a
Sandhya015 <sandhyanaik3249@gmail.com>
2024-07-18 00:28:57 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Job App',
+      title: 'HerAcademeia',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        scaffoldBackgroundColor: const Color.fromARGB(255, 75, 0, 130), // Set the background color to dark purple
+        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(159, 29, 5, 107)),
+        scaffoldBackgroundColor:Color.fromARGB(159, 29, 5, 107), // Set the background color to dark purple
         useMaterial3: true,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black,
-          iconTheme: IconThemeData(color: Colors.white),
-          titleTextStyle: TextStyle(color: Colors.white),
+          iconTheme: IconThemeData(color: Color.fromARGB(255, 20, 20, 20)),
+          titleTextStyle: TextStyle(color: Color.fromARGB(255, 5, 5, 5)),
         ),
       ),
       home: const MainPage(),
@@ -76,44 +76,61 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Women in Education', style: TextStyle(color: Colors.white)),
+        title: const Text('HerAcademeia', style: TextStyle(color: Colors.white)),
         centerTitle: true,
         backgroundColor: Colors.black, // Set AppBar color to black
       ),
-      body: const Center(
-        child: Text(
-          'Women Education: Empowering the Future',
-          style: TextStyle(fontSize: 24, color: Colors.white),
-          textAlign: TextAlign.center,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Women Education: Empowering the Future',
+              style: TextStyle(fontSize: 24, color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              'assets/download.jpeg',
+              width: 200,
+              height: 200,
+            ),
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.black, // Set BottomNavigationBar color to black
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home, color: Color.fromARGB(255, 12, 12, 12)),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info, color: Color.fromARGB(255, 13, 13, 13)),
-            label: 'About',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.work, color: Color.fromARGB(255, 18, 17, 17)),
-            label: 'Job',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school, color: Color.fromARGB(255, 7, 7, 7)),
-            label: 'Learn',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.white,
-        onTap: _onItemTapped,
-        // selectedLabelStyle: const TextStyle(color: Colors.black), 
-        // unselectedLabelStyle: const TextStyle(color: Colors.black),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          textTheme: Theme.of(context).textTheme.copyWith(
+                bodySmall: const TextStyle(color: Colors.black),
+              ),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: const Color.fromARGB(255, 244, 238, 238), // Set BottomNavigationBar color to black
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home, color: Color.fromARGB(255, 12, 12, 12)),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.info, color: Color.fromARGB(255, 13, 13, 13)),
+              label: 'About',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.work, color: Color.fromARGB(255, 18, 17, 17)),
+              label: 'Job',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school, color: Color.fromARGB(255, 7, 7, 7)),
+              label: 'Learn',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+          unselectedItemColor: const Color.fromARGB(255, 9, 9, 9),
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
 }
+
